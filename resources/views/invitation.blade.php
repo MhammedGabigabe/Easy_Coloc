31b09198355148f6f4b2767d5da0494b6fa230bb
Gabigabe <mhammed.gabigabe@gmail.com>
2026-03-01 03:17:16 +0000

    feat: ajout de la page invitation

---
+++ resources/views/invitation.blade.php
@@ -0,0 +1,7 @@
+<p>Bonjour ! </p> <br>
+
+<p>Vous avez été invité(e) à rejoindre une colocation sur EasyColoc. </p>
+<p>Cliquez sur ce lien pour répondre à l'invitation :</p>
+<a href="{{ route('invitation.response', ['token' => $data['token_email']]) }}">
+    Répondre à l'invitation
+</a>
