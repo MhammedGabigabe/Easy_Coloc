f4738524fe057e4b53b024336b095147384b89dc
Gabigabe <mhammed.gabigabe@gmail.com>
2026-03-01 03:30:08 +0000

    refactor: implementer de l'authentification d'un premier utilisateur sera Admin

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -35,16 +35,25 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        if(User::count() == 0){
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'role' => 'admin',
+                'password' => Hash::make($request->password),
+            ]);
+        }else{
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+        }
 
         event(new Registered($user));
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('colocations.index', absolute: false));
     }
 }
