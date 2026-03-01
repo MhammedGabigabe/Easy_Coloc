d2c58b8c94400c952cd0c4b485d9fe0d4db1d779
Gabigabe <mhammed.gabigabe@gmail.com>
2026-03-01 03:26:57 +0000

    refactor: modifier les routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Mail\InvitationMail;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\ColocationController;
+use App\Http\Controllers\InvitationController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,6 +14,22 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::post('/colocations', [ColocationController::class, 'store'])->name('colocations.store');
+Route::get('/colocations', [ColocationController::class, 'index'])->name('colocations.index');
+Route::get('/colocations/{id}', [ColocationController::class, 'show'])->name('colocations.show');
+Route::delete('/colocations/{id}', [ColocationController::class, 'destroy'])->name('colocations.destroy');
+
+Route::post('/invitation', [InvitationController::class, 'sendInvitation'])->name('invitation.invite');
+Route::get('/invitation/response/{token}', [InvitationController::class, 'showResponsePage'])
+    ->middleware(['auth'])
+    ->name('invitation.response');
+Route::post('/invitation/accept/{token}', [InvitationController::class, 'accept'])
+    ->middleware(['auth'])
+    ->name('invitation.accept');
+Route::post('/invitation/refuse/{token}', [InvitationController::class, 'refuse'])
+    ->middleware(['auth'])
+    ->name('invitation.refuse');        
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
