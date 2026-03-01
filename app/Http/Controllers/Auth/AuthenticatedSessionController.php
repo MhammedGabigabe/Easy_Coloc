1d93db193506f2f8234b34fce8776addf5e9927e
Gabigabe <mhammed.gabigabe@gmail.com>
2026-03-01 03:29:52 +0000

    refactor: implementer de l'authentification d'un premier utilisateur sera Admin

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,10 +28,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('colocations.index', absolute: false));
     }
 
-    /**
+    /** 
      * Destroy an authenticated session.
      */
     public function destroy(Request $request): RedirectResponse
