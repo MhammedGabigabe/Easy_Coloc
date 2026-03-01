0d35c8cca2383751fee761b195365d6d299fa933
Gabigabe <mhammed.gabigabe@gmail.com>
2026-03-01 03:22:18 +0000

    feat: ajout de le model Depense

---
+++ app/Models/Depense.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Depense extends Model
+{
+    protected $fillable = [
+        'titre', 
+        'montant', 
+        'createur_id', 
+        'colocation_id', 
+        'categorie_id', 
+        'date_depense'];
+
+    public function payeur()
+    {
+        return $this->belongsTo(User::class, 'createur_id');
+    }
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function categorie()
+    {
+        return $this->belongsTo(Categorie::class);
+    }
+
+    public function dettes()
+    {
+        return $this->hasMany(Dette::class);
+    }
+}
